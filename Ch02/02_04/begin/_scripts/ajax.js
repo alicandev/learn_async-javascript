c2b420d2cd1890f6f436d7c5dd681349cda04a08
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-19 15:17:32 +0000

    2.4 Working with a rejected promise

--- Ch02/02_04/begin/_scripts/ajax.js
+++ Ch02/02_04/begin/_scripts/ajax.js
@@ -56,15 +56,16 @@ function tempToF(kelvin) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  const apiKey = 'd126cacbbfebf7c84ad878e9deffc0e1';
-  //const apiKey = '';
-
-  const url =
-    'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' +
-    apiKey;
+  // const apiKey = 'd126cacbbfebf7c84ad878e9deffc0e1';
+  const apiKey = '';
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
   // get(url, successHandler, failHandler);
   // console.log(get(url));
-  get(url).then(function(response) {
-    successHandler(response);
-  });
+  get(url)
+    .then(function(response) {
+      successHandler(response);
+    })
+    .catch(function(status) {
+      failHandler(status);
+    });
 });
